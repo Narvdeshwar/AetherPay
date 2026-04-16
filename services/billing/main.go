9637bb28a73a1b8f6f35b51207434771fa0beb30
Sam Soni <sam.07soni@gmail.com>
2026-04-16 20:53:00 +0530

    subscription api

---
+++ services/billing/main.go
@@ -0,0 +1,57 @@
+package main
+
+import (
+	"aetherpay/shared"
+	"bytes"
+	"encoding/json"
+	"fmt"
+	"io/ioutil"
+	"log"
+	"net/http"
+
+	"github.com/gin-gonic/gin"
+)
+
+func main() {
+	app := gin.Default()
+	app.POST("/api/v1/billing/subscribe", func(c *gin.Context) {
+		paymentData := shared.PaymentRequest{
+			Name:     "Ashrith",
+			Amount:   2001.4,
+			Currency: "INR",
+		}
+		// convert data in json
+		reqData, err := json.Marshal(paymentData)
+		if err != nil {
+			c.JSON(http.StatusInternalServerError, gin.H{"error": "Failed to convert into the json"})
+			return
+		}
+		paymentURL := "http://localhost:3002/api/v1/payment/process"
+		resp, err := http.Post(paymentURL, "application/json", bytes.NewBuffer(reqData))
+		log.Println("Current resposnc from payments", resp, err)
+		if err != nil {
+			c.JSON(http.StatusInternalServerError, gin.H{"error": "Failed to load data"})
+			return
+		}
+		defer resp.Body.Close()
+
+		body, _ := ioutil.ReadAll(resp.Body)
+		var paymentResponse shared.PaymentResponse
+		json.Unmarshal(body, &paymentResponse)
+		log.Println(paymentResponse)
+		if paymentResponse.Status == "SUCCESS" {
+			c.JSON(http.StatusOK, gin.H{
+				"message":        "subscription sucessfull",
+				"transaction_id": paymentResponse.TransactionId,
+			})
+		} else {
+			c.JSON(http.StatusBadRequest, gin.H{
+				"message":        "payment failed",
+				"transaction_id": paymentResponse.TransactionId,
+			})
+		}
+
+	})
+	fmt.Println("Billing is runnig on the port 3001")
+	app.Run(":3001")
+}
