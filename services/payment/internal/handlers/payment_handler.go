2cc1646ba856293b7c83e9e57e987a5630e69c79
narvdeshwar <sam.07soni@gmail.com>
2026-04-28 15:24:08 +0530

    payment handler

--- services/payment/internal/handlers/payment_handler.go
+++ services/payment/internal/handlers/payment_handler.go
@@ -1,23 +1,37 @@
 package handlers
 
 import (
-	"aetherpay/payment/internal/repository"
+	service "aetherpay/payment/internal/services"
 	"aetherpay/shared"
+	"net/http"
 
 	"github.com/gin-gonic/gin"
 )
 
 type PaymentHandler struct {
-	Repo *repository.PaymentRepository
+	Service *service.PaymentService
 }
 
-func NewPaymentHandler(repo *repository.PaymentRepository) *PaymentHandler {
-	return &PaymentHandler{Repo: repo}
+func NewPaymentHandler(svc *service.PaymentService) *PaymentHandler {
+	return &PaymentHandler{Service: svc}
 }
 
 func (h *PaymentHandler) ProcessPayment(c *gin.Context) {
 	var req shared.PaymentRequest
+	
+	// 1. Input Check
 	if err := c.ShouldBindBodyWithJSON(&req); err != nil {
-		
+		c.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
+		return
 	}
+
+	// 2. Call The Brain (Service)
+	txnId, err := h.Service.ExecutePayment(req)
+	if err != nil {
+		c.JSON(http.StatusInternalServerError, gin.H{"error": "Failed to process payment"})
+		return
+	}
+
+	// 3. Success Response
+	c.JSON(http.StatusOK, shared.PaymentResponse{TransactionId: txnId, Status: "SUCCESS"})
 }
