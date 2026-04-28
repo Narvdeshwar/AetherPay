aa525c82134d39ca50a8f90b36a23ce041973771
narvdeshwar <sam.07soni@gmail.com>
2026-04-28 16:56:17 +0530

    updated the billing

--- services/billing/cmd/api/main.go
+++ services/billing/cmd/api/main.go
@@ -1,58 +1,31 @@
 package main
 
 import (
-	"aetherpay/shared"
-	"bytes"
-	"encoding/json"
+	"aetherpay/billing/internal/clients"
+	"aetherpay/billing/internal/handlers"
+	service "aetherpay/billing/internal/services"
 	"fmt"
-	"io/ioutil"
-	"log"
-	"net/http"
 
 	"github.com/gin-gonic/gin"
 )
 
 func main() {
+	// 1. Initialize HTTP Client
+	// Docker mein 'payment-service' container name use hoga
+	paymentClient := clients.NewPaymentClient("http://payment-service:3002")
+
+	// 2. Initialize Service & Handler
+	svc := service.NewBillingService(paymentClient)
+	handler := handlers.NewBillingHandler(svc)
+
+	// 3. Setup Router
 	app := gin.Default()
-	app.POST("/api/v1/billing/subscribe", func(c *gin.Context) {
-		paymentData := shared.PaymentRequest{
-			Name:     "Ashrith",
-			Amount:   2001.4,
-			Currency: "INR",
-			UserId:   "123",
-		}
-		// convert data in json
-		reqData, err := json.Marshal(paymentData)
-		if err != nil {
-			c.JSON(http.StatusInternalServerError, gin.H{"error": "Failed to convert into the json"})
-			return
-		}
-		paymentURL := "http://payment-service:3002/api/v1/payment/process"
-		resp, err := http.Post(paymentURL, "application/json", bytes.NewBuffer(reqData))
-		log.Println("Current resposnc from payments", resp, err)
-		if err != nil {
-			c.JSON(http.StatusInternalServerError, gin.H{"error": "Failed to load data"})
-			return
-		}
-		defer resp.Body.Close()
+	api := app.Group("/api/v1")
+	{
+		api.POST("/billing/subscribe", handler.Subscribe)
+	}
 
-		body, _ := ioutil.ReadAll(resp.Body)
-		var paymentResponse shared.PaymentResponse
-		json.Unmarshal(body, &paymentResponse)
-		log.Println(paymentResponse)
-		if paymentResponse.Status == "SUCCESS" {
-			c.JSON(http.StatusOK, gin.H{
-				"message":        "subscription sucessfull",
-				"transaction_id": paymentResponse.TransactionId,
-			})
-		} else {
-			c.JSON(http.StatusBadRequest, gin.H{
-				"message":        "payment failed",
-				"transaction_id": paymentResponse.TransactionId,
-			})
-		}
-
-	})
-	fmt.Println("Billing is runnig on the port 3001")
+	// 4. Start Server
+	fmt.Println("🧾 Billing Service running on port 3001")
 	app.Run(":3001")
 }
