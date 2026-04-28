bd38aec7e5fbf0e8ed4201adffd12549d6f69d3b
narvdeshwar <sam.07soni@gmail.com>
2026-04-28 16:53:12 +0530

    billing services

--- services/billing/internal/services/billing_service.go
+++ services/billing/internal/services/billing_service.go
@@ -1,1 +1,26 @@
-package services
+package service
+
+import (
+	"aetherpay/billing/internal/clients"
+	"aetherpay/shared"
+)
+
+type BillingService struct {
+	PaymentClient *clients.PaymentClient
+}
+
+func NewBillingService(pc *clients.PaymentClient) *BillingService {
+	return &BillingService{PaymentClient: pc}
+}
+
+func (s *BillingService) SubscribeUser(req shared.PaymentRequest) (*shared.PaymentResponse, error) {
+	// Yahan aap future mein check kar sakte hain ki User pehle se subscribed toh nahi hai
+	
+	// Payment process karne ke liye client ko call karein
+	resp, err := s.PaymentClient.ProcessPayment(req)
+	if err != nil {
+		return nil, err
+	}
+
+	return resp, nil
+}
