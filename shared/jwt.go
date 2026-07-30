8c08ce8fe17baeb5e4c981a874ddbe33b965b7dd
narvdeshwar <sam.07soni@gmail.com>
2026-07-30 10:01:01 +0000

    Validate the jwt token

--- shared/jwt.go
+++ shared/jwt.go
@@ -1,6 +1,7 @@
 package shared
 
 import (
+	"errors"
 	"time"
 
 	"github.com/golang-jwt/jwt/v5"
@@ -26,4 +27,21 @@ func GenerateJWT(userId, tenantId, role, secret string, duration time.Duration) 
 	}
 	token := jwt.NewWithClaims(jwt.SigningMethodHS256, claims)
 	return token.SignedString([]byte(secret))
+}
+
+func ValidateJWT(tokenString, secret string) (*CustomClaims, error) {
+	token, err := jwt.ParseWithClaims(tokenString, &CustomClaims{}, func(t *jwt.Token) (interface{}, error) {
+		if _, ok := t.Method.(*jwt.SigningMethodHMAC); !ok {
+			return nil, errors.New("unexpected signing method")
+		}
+		return []byte(secret), nil
+	})
+	if err != nil {
+		return nil, err
+	}
+	claims, ok := token.Claims.(*CustomClaims)
+	if !ok || !token.Valid {
+		return nil, errors.New("invalid or expired token")
+	}
+	return claims, nil
 }
