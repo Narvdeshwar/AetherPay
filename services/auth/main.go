b95a7831d21f5c9696b87949339fde4c91d34c33
narvdeshwar <sam.07soni@gmail.com>
2026-07-30 12:21:46 +0000

    auth handler for login added

--- services/auth/main.go
+++ services/auth/main.go
@@ -1,14 +1,3 @@
 package main
 
-import (
-	"fmt"
 
-	"github.com/Narvdeshwar/AetherPay/shared"
-)
-
-func main() {
-	tenantID := "test_user_101"
-	err := shared.NewAPIError(401, "Invalid JWT Token", tenantID)
-	fmt.Println("Auth service running")
-	fmt.Println(err.Error())
-}
