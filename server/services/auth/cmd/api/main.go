cf19b9fb831303e6d716a3e2fa735a3bb9427a1e
Sam Soni <sam.07soni@gmail.com>
2026-09-04 13:35:26 +0530

    main.go connected configuration done

--- server/services/auth/cmd/api/main.go
+++ server/services/auth/cmd/api/main.go
@@ -1,13 +1,33 @@
 package main
 
 import (
-	"fmt"
+	"log"
+	"os"
 
 	"github.com/Narvdeshwar/AetherPay/services/auth/internal/config"
+	"github.com/Narvdeshwar/AetherPay/services/auth/internal/handler"
+	"github.com/Narvdeshwar/AetherPay/services/auth/internal/repository"
+	"github.com/gin-gonic/gin"
 )
 
 func main() {
+
 	cfg := config.LoadConfig()
 	db := config.InitDB(cfg)
-	fmt.Println(db)
+
+	merchantRepo := repository.NewMerchantRepository(db)
+
+	authHandler := handler.NewAuthHandler(merchantRepo, os.Getenv("JWT_SECRET"), 15)
+
+	r := gin.Default()
+	v1 := r.Group("/api/v1/auth")
+	{
+		v1.POST("/register", authHandler.Register)
+		v1.POST("/login", authHandler.Login)
+	}
+	log.Println("auth Service is running on port 3001")
+	if err := r.Run(os.Getenv("AUTH_PORT")); err != nil {
+		log.Fatalf("Error running the auth server %v", err)
+	}
+
 }
---
+++ server/services/auth/internal/handler/auth_handler.go
@@ -0,0 +1,41 @@
+package handler
+
+import (
+	"time"
+
+	"github.com/Narvdeshwar/AetherPay/services/auth/internal/repository"
+	"github.com/gin-gonic/gin"
+)
+
+type AuthHandler struct {
+	repo      repository.MerchantRepository
+	jwtSecret string
+	tokenTTL  time.Duration
+}
+
+// constructor
+func NewAuthHandler(repo repository.MerchantRepository, jwtSecret string, tokenTTL time.Duration) *AuthHandler {
+	return &AuthHandler{
+		repo:      repo,
+		jwtSecret: jwtSecret,
+		tokenTTL:  tokenTTL,
+	}
+}
+
+type RegisterRequest struct {
+	Email        string `json:"email" binding:"required,email"`
+	Password     string `json:"password" binding:"required,min=8"`
+	MerchantName string `json:"merchant_name" binding:"required,min=2,max=255"`
+}
+
+type LoginRequest struct {
+	Email    string `json:"email" binding:"required,email"`
+	Password string `json:"password" binding:"required,min=8"`
+}
+
+func (h *AuthHandler) Register(c *gin.Context) {
+
+}
+func (h *AuthHandler) Login(c *gin.Context) {
+
+}
